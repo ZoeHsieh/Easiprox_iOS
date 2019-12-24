b9f90a3ea06dceed10920c8cbd684467f1d0933b
“Yusi <zoehsieh0924@gmail.com>
2019-12-24 11:37:35 +0800

    1224

--- E3AK/AppDelegate.swift
+++ E3AK/AppDelegate.swift
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
        
-
+        UIApplication.shared.isIdleTimerDisabled = true //恆亮1224
         let isfirst = Config.saveParam.bool(forKey: Config.firstOpen)
         var storyboard:UIStoryboard!
         
